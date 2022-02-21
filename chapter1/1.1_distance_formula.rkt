dfb843bd1fe7af64b9a9174252c674a88d68fd2c
sararekowska <sara.rekowska@gmail.com>
2022-02-21 20:20:42 +0100

    1.2/1.3

---
+++ chapter1/1.1_distance_formula.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 1.1_distance_formula) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define x 12)
+(define y 5)
+(define distance (sqrt ( + (sqr x) (sqr y))))
+(string-append "distance: " (number->string distance))
---
+++ chapter1/1.3_substr.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 1.3_substr) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; (substring "hello world" 1 5)
+; "ello"
+
+(define str "helloworld")
+(define i 5)
+
+(string-append (substring str 0 i)
+               "_"
+               (substring str i 10))
